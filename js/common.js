735aadcfd6f3ffbc2c70862acb9ea613db65f0a1
Turgenoid <turgenoid@gmail.com>
2018-01-08 22:05:54 +0300

    Build 6

--- js/common.js
+++ js/common.js
@@ -6,7 +6,41 @@ $(document).ready(function(){
   	$('.mobile-menu').toggleClass('mobile-menu-active');
   });
 
-  $('.owl-carousel').owlCarousel({
-  	items: 1
+  // CATALOG 
+  var carousel = $('.owl-carousel');
+  var cat_number;
+  var cat_name
+
+  carousel.owlCarousel({
+  	items: 1,
+  	autoHeight: true,
+    autoplay: true,
+    margin: 10,
+    autoplayTimeout: 10000,
+    autoplayHoverPause: true,
+    autoplaySpeed: 2500,
+  	onInitialized: setCatName,
+  	onChanged: setCatName
   });
+
+  function setCatName(event) {
+  	cat_number = event.item.index;
+  	cat_name = $('.fullwidth-menu ul li:eq(' + cat_number + ') a').html();
+  	$('.catalog .content > h2').html(cat_name);
+
+  	$('.fullwidth-menu ul li:eq(' + cat_number + ') a').toggleClass('accent-text-color'); 
+
+  	for(var i = 0; i < event.item.count; i++) {
+  		if(i !== cat_number) {
+  			cat_name = $('.fullwidth-menu ul li:eq(' + i + ') a').removeClass();
+  		}
+  	}
+  }
+
+ 	$('.fullwidth-menu ul li a').click(function(event) {
+  	var cat_link = event.target.hash;
+  	cat_number = cat_link.split('#cat')[1] - 1;
+
+  	carousel.trigger('to.owl.carousel', cat_number);
+ 	});
 });
